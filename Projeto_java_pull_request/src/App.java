6ca4bad308f8b07aba8b9228ebad29d96fb9f6d0
lcostaju <luciocj_eng@outlook.com>
2024-09-08 14:29:45 -0300

    criação de classe

---
+++ Projeto_java_pull_request/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 3;
+        int b = 2;
+        int produto = a * b;
+
+        System.out.println(produto);
+    }
+}
